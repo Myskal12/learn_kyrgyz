638a482c71031f0afcb4ba0e8b8e533064d5a327
Myskal <myskal12@users.noreply.github.com>
2026-04-12 13:11:22 +0600

    Deploy Flutter web to GitHub Pages

--- android/app/src/main/kotlin/com/example/learn_kyrgyz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.learn_kyrgyz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/utils/constants.dart
+++
@@ -1,3 +0,0 @@
-class Constants {
-  static const appName = 'English from Kyrgyz';
-}
